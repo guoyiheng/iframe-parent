374c0ddb875a2105337df7b7214914e3906adb48
guoyiheng <hi@gyh.cool>
2023-08-14 14:39:38 +0800

    .

--- src/pages/iframe.vue
+++ src/pages/iframe.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-const childUrI = 'https://iframe-child-1.vercel.app/test'
+const childUrI = 'https://iframe-child-1.vercel.app'
+function refreshPage() {
+  window.location.reload()
+}
 </script>
 
 <template>
-  <div>
-    <iframe :src="childUrI" frameborder="0"></iframe>
+  <div border-1px p-10>
+    <iframe :src="childUrI" frameborder="0" />
+    <div>Parent Iframe</div>
+    <button
+    btn
+      @click="refreshPage"
+    >
+      Refresh page
+    </button>
   </div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,37 +10,33 @@ function go() {
   if (name.value)
     router.push(`/hi/${encodeURIComponent(name.value)}`)
 }
+
+const childUrI = 'https://iframe-child-1.vercel.app'
+function refreshPage() {
+  window.location.reload()
+}
 </script>
 
 <template>
-  <div>
-    <div i-carbon-campsite inline-block text-4xl />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
+  <div class="border-1px" p-10>
+    <iframe class="h-300px w-full" :src="childUrI" frameborder="0" />
 
-    <div py-4 />
-
-    <TheInput
+    <div m-3>
+      Parent Iframe
+    </div>
+    <button btn @click="refreshPage">
+      Refresh page
+    </button>
+    <!-- <TheInput
       v-model="name"
       placeholder="What's your name?"
       autocomplete="false"
       @keydown.enter="go"
     />
-
     <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
+      <button class="m-3 text-sm btn" :disabled="!name" @click="go">
         Go
       </button>
-    </div>
+    </div> -->
   </div>
 </template>
