4cfc34886f9f2ee65f68a2c1f8675072951dda02
guoyiheng <hi@gyh.cool>
2023-08-14 14:22:49 +0800

    .

---
+++ src/pages/iframe.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const childUrI = 'https://iframe-child-1.vercel.app/test'
+</script>
+
+<template>
+  <div>
+    <iframe :src="childUrI" frameborder="0"></iframe>
+  </div>
+</template>
